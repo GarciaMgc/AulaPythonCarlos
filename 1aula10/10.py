0d87961773ad5f66962dc9d78e2955df09d1426f
GarciaMgc <114542432+GarciaMgc@users.noreply.github.com>
2024-12-09 17:16:50 -0300

    oioi

---
+++ 1aula10/10.py
@@ -0,0 +1,53 @@
+
+
+# print('Hello mundinho!')
+
+# # comentario
+# # comentar
+# # hablar (para comentar)
+
+# """
+# bloco para explicar funções (notações). 
+# """
+
+# # variaveis são espaços reservados na memoria para guardar valores
+
+# #string
+# nome="lilla"
+
+# #numeros
+# idade=22 #inteiro
+# altura=1.57 #float (é um tipo de dado que armazena números reais, ou seja, números que possuem casas decimais)
+
+# #booleans
+# verdadeiro=True
+# falso=False
+
+# #sequencia
+# lista=[123,True, "lilla", 1.57, ["outra, lista"]] #array do js
+# tupla=(123, True, "lilla", 1.57)
+# dicionarios = {"nome": "lilla", "idade":22}
+
+# valores_none= None
+
+# teste=18
+# print(teste)
+# teste='carlos'
+# print(teste)
+
+# teste_teste = 'carlos' #snack case
+
+# salario_base = 8.000
+# gratificacao = input("Digite o valor da gratificação: ") #por padrão uma string
+# gratificacao = int(input("Digite o valor da gratificação: ")) 
+
+# #processamento
+# salario_final = salario_base + gratificacao
+
+# #saida
+# print("O salario final é de: ", salario_final)
+
+# print("Salario final é de: ", salario_final) #1
+# print("SALARIO {} final é de: {}".format(salario_final, salario_base)) #2
+# print(f"O salario final é de {salario_final} e o cara ta liso no fim do mês!") #3
+
